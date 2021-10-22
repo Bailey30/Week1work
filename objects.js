0e04c8c5a9064c8b165c976ff80fd0009163ddfa
Alex Bailey <baileymk@hotmail.com>
2021-10-22 14:17:33 +0100

    updated activity 3 - coffeeShop

--- objects.js
+++ objects.js
@@ -42,49 +42,57 @@
 // pet.drink()
 
 // activity 3 - coffee shop
-let order1 = []
-let order2 = []
+
 const coffeeShop = {
     branch: "wood",
     drinks: [
         ["water"],
         ["1.5"],
         ["iced water"],
-        ["2.5"],
+        ["2.54"],
         ["tea"],
         ["2"]
     ],
     food: {
         bread: 1,
-        rice: 2
+        rice: 2.5,
+        oats: 2.7,
+        potato: 3.43
     },
     drinksOrdered(drink1, drink2) {
 
         // take items from array object and put into another array
         let result = coffeeShop.drinks.map(element => element.join())
 
-        let result2 = coffeeShop.drinks.map(element => element)
-
-        let i3 = result2.indexOf(drink1)
-
         let i1 = result.indexOf(drink1)
         let i2 = result.indexOf(drink2)
         let m1 = result.indexOf(drink1) + 1
         let m2 = result.indexOf(drink2) + 1
 
-        let price = (parseFloat(result[m1])) + (parseFloat(result[m2]))
-
-        console.log(`your order is ${result[i1]} and ${result[i2]}. your total is ${price}`);
+        let price = parseFloat(((result[m1]) + (result[m2])))
+        let pricefixed = price.toFixed(2)
+        console.log(`your order is ${result[i1]} and ${result[i2]}. your total is £${pricefixed}`);
 
 
     },
-    foodOrdered() {
-        const object = this.food
-        for (const property in object) {
-            console.log(`${property}: ${object[property]}`);
-        }
+    foodOrdered(food1, food2) {
+       
+        //Object.keys and Object.values takes the keys and value of an object and put them into an array
 
+        let food = Object.keys(coffeeShop.food);
+        let prices = Object.values(coffeeShop.food)
+
+        //using indexOf to find the index of the foods in the array we just created. Index is later used to find the food in the array depending on what arguements are passed when calling the function
+        let f1 = food.indexOf(food1)
+        let f2 = food.indexOf(food2)
+
+        //index of food matches index of drink array so food name index is used find price and put in an equation
+        let price = parseFloat(((prices[f1]) + (prices[f2])).toFixed(2))
+        
+        console.log(`your order is ${food[f1]} and ${food[f2]}. your total is £${price}`);
+       
     }
 }
 console.log(coffeeShop.drinksOrdered("iced water", "water"))
-coffeeShop.foodOrdered()
+coffeeShop.foodOrdered("oats", "potato")
+
